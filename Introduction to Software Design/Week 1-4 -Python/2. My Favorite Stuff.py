a236303e167fffc6af6655ae64a12b9b42dd9a4c
XtraAzure <35952292+XtraAzure@users.noreply.github.com>
2018-03-14 17:34:06 +0900

    Add files via upload

---
+++ Introduction to Software Design/Week 1-4 -Python/2. My Favorite Stuff.py
@@ -0,0 +1,10 @@
+print 'Tell me what your favorite color is.'
+color = raw_input()
+print 'Tell me what your favorite animal is.'
+animals = raw_input()
+print 'Tell me what your favorite food is.'
+food = raw_input()
+print 'You entered: ' + food + ' ' + animals + ' ' + color
+print 'Color: ' + color
+print 'Animal: ' + animals
+print 'Food: ' + food
---
+++ Introduction to Software Design/Week 1-4 -Python/4. Jokes.py
@@ -0,0 +1,30 @@
+print 'What do you get when you cross a snowman with a vampire?'
+FirstAnswer = raw_input()
+print 'Frostbite!\n'
+print 'What do dentists call a astronaut' + '\'s ' + 'cavity?'
+SecondAnswer = raw_input()
+print 'A black hole! \n'
+print 'Knock Knock.\n'
+print 'Who\'s there?\n'
+print 'Interrupting cow.\n'
+print 'Interrupting cow wh MOO!'
+
+'''
+Answer:
+print 'What do you get when you cross a snowman with a vampire?'
+raw_input()
+print 'Frostbite!'
+print
+print 'What do dentists call a astronaut/'s cavity?'
+raw_input()
+print 'A black hole!'
+print
+print 'Knock Knock.'
+raw_input()
+print 'Who\'s there?'
+raw_input()
+print 'Interrupting cow.'
+raw_input()
+print 'Interrupting cow wh ',
+print 'MOO!'
+'''
